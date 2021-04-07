800ae3985ceec2da37400b348be21a1f947e8711
Yash K <55628921+Yashrk078@users.noreply.github.com>
2021-04-07 12:59:29 +0530

    Update nuclei.sh

--- nuclei.sh
+++ nuclei.sh
@@ -7,6 +7,12 @@ do
 done
 
 nuc(){
-		cat $domain/alive.txt | nuclei -t ~/nuclei-templates -o $domain/recon/nuclei/nuc.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/cves/ -o $domain/recon/nuclei/cves.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/exposures/ -o $domain/recon/nuclei/exposures.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/vulnerabilities/ -o $domain/recon/nuclei/vulns.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/misconfiguration/ -o $domain/recon/nuclei/misconfig.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/exposed-panels/ -o $domain/recon/nuclei/panels.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/takeovers/ -o $domain/recon/nuclei/takeovers.txt
+		cat $domain/alive.txt | nuclei -t ~/nuclei-templates/workflows/ -o $domain/recon/nuclei/workflows.txt
 		}
 nuc
